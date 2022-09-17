9b44d5a1e0a95d366cea09dd623a890d3a5de6eb
MahmoudFarid <mahmoudfarid931@gmail.com>
2022-09-17 18:52:23 +0200

    responsive for different devices

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primary=Color(0xFF195176);
+  static const Color withdrawColor=Colors.redAccent;
+  static const Color depositColor=Colors.green;
+  static const Color textColor1=Colors.white;
+  static const Color textColor2=Colors.black;
+  static const Color greyText=Colors.grey;
+  static const Color buttonsColor=Colors.blueAccent;
+}
---
+++ lib/core/utils/assets_manager.dart
@@ -0,0 +1,14 @@
+const String imagePath='assets/images';
+
+class AssetManager{
+  static String arabicImg='$imagePath/ar_EG.png';
+  static String englishImg='$imagePath/en_US.png';
+  static String spanishImg='$imagePath/es_ES.jpg';
+  static String depositImg='$imagePath/deposit.png';
+  static String withdrawImg='$imagePath/withdraw.png';
+  static String emptyListImg='$imagePath/emptylist.png';
+  static String errorScreenImg='$imagePath/errorScreen.png';
+  static String appLogo='$imagePath/lasfinanzas.png';
+  static String splashScreenImg='$imagePath/splashimage.png';
+
+}
